5e13fba78e608251ecf42d16e1ea3337e263579e
csengekulker <k71608883429@ktch.hu>
2022-09-05 09:13:13 +0200

    implements

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("xd");
+
+        Kutya cica = new Kutya();
+        cica.ugat();
     }
 }
---
+++ Kutya.java
@@ -0,0 +1,5 @@
+public class Kutya implements Allat{
+    public void ugat() {
+        System.out.println("vau vau");
+    }
+}
